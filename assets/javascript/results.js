e856abec102da43c93dcfd5a1d25d68240ac5994
Melodie Chi <Melodie@Apple.local>
2017-11-10 00:54:10 -0800

    everything is working, but need chrome CORS extension...

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -1,90 +1,186 @@
-// // Initialize Firebase
-// var config = {
-// 	apiKey: "AIzaSyAllfQG9oAzeZk-W_f3aW_f3dYw6Axq4gk",
-// 	authDomain: "project-1-8e126.firebaseapp.com",
-// 	databaseURL: "https://project-1-8e126.firebaseio.com",
-// 	projectId: "project-1-8e126",
-// 	storageBucket: "",
-// 	messagingSenderId: "704064336079"
-// };
-// firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAllfQG9oAzeZk-W_f3aW_f3dYw6Axq4gk",
+	authDomain: "project-1-8e126.firebaseapp.com",
+	databaseURL: "https://project-1-8e126.firebaseio.com",
+	projectId: "project-1-8e126",
+	storageBucket: "",
+	messagingSenderId: "704064336079"
+};
+firebase.initializeApp(config);
 
 
-// // GLOBAL VARIABLES
-// // ====================================================================================
-// var database = firebase.database();
-// var userName = "";
-// var userCompany = "";
-// var userDressCode = "";
-// var userIndustry = "";
-// var userOutfit = "";
+// GLOBAL VARIABLES
+// ====================================================================================
+var database = firebase.database();
+var userName = "";
+var userCompany = "";
+var userDressCode = "";
+var userIndustry = "";
+var userOutfit = "";
 
 
-// // FUNCTIONS
-// // ====================================================================================
-// // function to render new row on DOM from firebase
-// function displayNewRow(snapshotObject) {
+// FUNCTIONS
+// ====================================================================================
+// function to render new row on DOM from firebase
+function displayNewRow(array) {
 
-// 	// dynamically create a virtual table row
-// 	var newRow = $("<tr>");
+    // dynamically create a virtual table row
+    var newRow = $("<tr class='new-row'>");
 
-// 	// for each item in the array
-// 	for (var i = 0; i < userArray.length; i++) {
+    // for each item in the array
+    for (var i = 0; i < array.length; i++) {
 
-// 		// append its table data to the virtual row
-// 		newRow.append("<td>" + userArray[i] + "</td>");
-// 	}
+        // append its table data to the virtual row
+        newRow.append("<td>" + array[i] + "</td>");
+    }
 
-// 	// append the row to the DOM
-// 	$("tbody").append(newRow);
+    // append the row to the DOM
+    $("tbody").prepend(newRow);
 
-// }
+}
 
-// // function to initialize page with existing data in firebase
-// function initPage(snapshotObject) {
+// function to initialize page with existing data in firebase
+function initPage(snapshotObject) {
 
-// 	// if there is data stored in database when page loads
-// 	if (snapshotObject.numChildren() >= 1) {
+    // if there is data stored in database when page loads
+    if (snapshotObject.numChildren() >= 1) {
 
-// 		// for each object
-// 		snapshotObject.forEach(function(childSnapshot) {
+        // for each object
+        snapshotObject.forEach(function(childSnapshot) {
 
-// 			// make an array for the value of each property in the object
-// 			var userArray = [childSnapshot.child("name").val(), childSnapshot.child("company").val(), childSnapshot.child("dressCode").val(), childSnapshot.child("industry").val()];
-// 			//childSnapshot.child("outfitResult").val()
+            // make an array for the value of each property in the object
+            var userArray = [childSnapshot.child("name").val(), childSnapshot.child("company").val(), childSnapshot.child("dressCode").val(), childSnapshot.child("industry").val(), childSnapshot.child("outfitResult").val()];
+            //childSnapshot.child("outfitResult").val()
 
-// 			// display to page (append to table)
-// 			displayNewRow(userArray);
+            // display to page (append to table)
+            displayNewRow(userArray);
 
-// 		});
+        });
 
-// 	} else { 
-// 		return;
-// 	}
+    } else {
+        return;
+    }
 
-// }
+}
+
+// function to display appropriate results to DOM
+function displayCorrectResults(snapshotObject) {
+
+	var snapshotResult = snapshotObject.val().outfitResult;
+
+	// display correct outfit result
+	$("#outfit-result").html(snapshotResult);
+
+	// display google maps for if result is casual
+	displayGoogleMaps(snapshotResult);
+
+	// display correct modal button icon
+	displayModalBtnIcon(snapshotResult);
+
+	// display correct modal message
+	displayModalMessage(snapshotResult);
+
+}
+
+// function to display user img to DOM
+function displayImg(snapshotObject) {
+	var snapshotImg = snapshotObject.val().imgb64;
+	$("#outfit-preview").attr("src", snapshotImg);
+}
+
+// function to display google maps if result is casual
+function displayGoogleMaps(result) {
+
+	if (result === "Casual") {
+		$("#google-maps").show();
+	}
+	else if (result === "Formal") {
+		$("#google-maps").hide();
+	}
+
+}
+
+// function to display sad face or happy face in modal button based on result
+function displayModalBtnIcon(result) {
+
+	if (result === "Formal") {
+		$("#happy-face").show();
+		$("#sad-face").hide();
+	}
+	else if (result === "Casual") {
+		$("#sad-face").show();
+		$("#happy-face").hide();
+	}
+
+}
+
+// function to display appropriate modal message based on result
+function displayModalMessage(result) {
+
+	if (result === "Formal") {
+		$("#formal-message").show();
+		$("#casual-message").hide();
+	}
+	else if (result === "Casual") {
+		$("#casual-message").show();
+		$("#formal-message").hide();
+	}
+
+}
+
+// function for hiding rows once data storage gets past 5 people
+function hideRows() {
+    $(".new-row").slice(5).css("display", "none");
+}
 
 
-// // MAIN PROCESS
-// // ====================================================================================
-// // when page loads
-// database.ref().once("value", function(snapshot) {
+// MAIN PROCESS
+// ====================================================================================
+$(window).on("change", function() {
 
-// 	// initialize table
-// 	initPage(snapshot);
+    // flash user's new row on results page
+    $(".new-row:first-child").addClass("flash");
 
-// // error handling
-// }, function(err) {
-// 	console.error(err);
-// });
+    setTimeout(function(){ 
+        $(".new-row:first-child").removeClass("flash");
+    }, 1000);
 
-// // when new object is pushed to firebase
-// database.ref().on("child_added", function(snapshot) {
+})
 
-// 	// create new row on DOM from all user input
-// 	displayNewRow(snapshot);
+// when page loads
+database.ref().once("value", function(snapshot) {
 
-// // error handling
-// }, function(err) {
-// 	console.error(err);
-// });
+    // initialize table
+    initPage(snapshot);
+
+    // hide rows past row 5
+    hideRows();
+
+    // error handling
+}, function(err) {
+    console.error(err);
+});
+
+// whenever a new object is pushed to firebase
+database.ref().on("child_added", function(snapshot) {
+
+    // display outfit result
+    displayCorrectResults(snapshot);
+
+    // display user outfit img
+    displayImg(snapshot);
+
+    // flash user's new row on results page
+    $(".new-row:first-child").addClass("flash");
+
+    // error handling
+}, function(err) {
+    console.error(err);
+});
+
+// only console out most recent object from firebase
+firebase.database().ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+    // console insight
+    console.log(snapshot.val());
+});
--- assets/javascript/submit.js
+++ assets/javascript/submit.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyAllfQG9oAzeZk-W_f3aW_f3dYw6Axq4gk",
-	authDomain: "project-1-8e126.firebaseapp.com",
-	databaseURL: "https://project-1-8e126.firebaseio.com",
-	projectId: "project-1-8e126",
-	storageBucket: "",
-	messagingSenderId: "704064336079"
+    apiKey: "AIzaSyAllfQG9oAzeZk-W_f3aW_f3dYw6Axq4gk",
+    authDomain: "project-1-8e126.firebaseapp.com",
+    databaseURL: "https://project-1-8e126.firebaseio.com",
+    projectId: "project-1-8e126",
+    storageBucket: "",
+    messagingSenderId: "704064336079"
 };
 firebase.initializeApp(config);
 
@@ -22,136 +22,107 @@ var userOutfit = "";
 
 // FUNCTIONS
 // ====================================================================================
-// function to render new row on DOM from firebase
-function displayNewRow(array) {
 
-	// dynamically create a virtual table row
-	var newRow = $("<tr class='new-row'>");
+// //console.log(JSON.stringify(image));
+// $('#file-input').change(function(event) {
+//   	console.log(event.target.files);
+//     var tmppath = URL.createObjectURL(event.target.files[0]);
+//     console.log(tmppath);
+// });
 
-	// for each item in the array
-	for (var i = 0; i < array.length; i++) {
+function requestVizeAPI() {
 
-		// append its table data to the virtual row
-		newRow.append("<td>" + array[i] + "</td>");
+	// if all fields are not filled out
+	 userName = $("#name").val();
+				    userCompany = $("#company").val();
+				    userDressCode = $("#dress-code").val();
+				    userIndustry = $("#industry").val();
+				    userOutfit = $("#outfit").val();
+	if (userName === "" ||
+		userCompany === "" ||
+		userDressCode === "" ||
+		userIndustry === "" ||
+		userOutfit === "") {
+		debugger;
+		console.log("problemo")
+		// trigger modal
+        $('#submit').addClass("modal-trigger");
+    }
+    else {
+    	$("#submit").removeClass("modal-trigger");
+	    $("#loading").show();
+	    $("#main").hide();
+	    var selectedFile = document.getElementById('file-input').files[0];
+	    var path = URL.createObjectURL(selectedFile);
+	    var fr = new FileReader();
+	    fr.onload = function(e) {
+	        console.log("LOADED");
+	        $("#preview").attr("src", e.target.result);
+	        // capture push function so that myKey returns the key of the specific object pushed
+	        console.log('http://cl-api.vize.ai/3313?image=' + $("#preview").attr("src"));
+
+	        var image = new FormData();
+	        image.append("image", selectedFile);
+	        console.log(image);
+
+
+	        axios.post('http://cl-api.vize.ai/3313', image,
+
+	                {
+	                    "headers": {
+	                        "Authorization": "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjMxODQsImlhdCI6MTUxMDEwNjQ2MiwiZXhwIjoxNTE3ODgyNDYyfQ.w4vK7xPw-Fff7vWbKGFe1AqB-2Od-lCnsR8KHU3bVow",
+	                    }
+	                }
+	            )
+	            .then(function(response) {
+	                console.log(response);
+	                console.log(response.data.prediction);
+
+	                // capture all user input to push to firebase
+				    userName = $("#name").val();
+				    userCompany = $("#company").val();
+				    userDressCode = $("#dress-code").val();
+				    userIndustry = $("#industry").val();
+				    userResult = response.data.prediction;
+
+				    // create array from inputs to be used by displayNewRow function
+				    var userArray = [userName, userCompany, userDressCode, userIndustry, userResult];
+
+			        // create object to push to firebase
+			        var pushObject = {
+			            name: userName,
+			            company: userCompany,
+			            dressCode: userDressCode,
+			            industry: userIndustry,
+			            outfitResult: userResult,
+			            imgb64: e.target.result,
+			            timestamp: firebase.database.ServerValue.TIMESTAMP
+			        }
+			        var myKey = database.ref().push(pushObject);
+
+	                // redirect to results page & when api call is done append file parameters to URL
+	                window.location = "results.html?id=" + myKey.key + "&dressType=" + response.data.prediction;
+
+	            })
+	            .catch(function(error) {
+	                console.log(error);
+	            });
+	    }
+	    fr.readAsDataURL(selectedFile)
+	    // fr.error(function(err) {
+
+	    // })
 	}
-
-	// append the row to the DOM
-	$("tbody").prepend(newRow);
-
-}
-
-// function to initialize page with existing data in firebase
-function initPage(snapshotObject) {
-
-	// if there is data stored in database when page loads
-	if (snapshotObject.numChildren() >= 1) {
-
-		// for each object
-		snapshotObject.forEach(function(childSnapshot) {
-
-			// make an array for the value of each property in the object
-			var userArray = [childSnapshot.child("name").val(), childSnapshot.child("company").val(), childSnapshot.child("dressCode").val(), childSnapshot.child("industry").val()];
-			//childSnapshot.child("outfitResult").val()
-
-			// display to page (append to table)
-			displayNewRow(userArray);
-
-		});
-
-	} else { 
-		return;
-	}
-
-}
-
-// function for when user submits form
-function submit() {
-
-	// capture all user input
-	userName = $("#name").val();
-	userCompany = $("#company").val();
-	userDressCode = $("#dress-code").val();
-	userIndustry = $("#industry").val();
-	userOutfit = $("#outfit").val();
-
-	// create object to push to firebase
-	var pushObject = {
-		name: userName,
-		company: userCompany,
-		dressCode: userDressCode,
-		industry: userIndustry,
-		outfit: userOutfit,
-		timestamp: firebase.database.ServerValue.TIMESTAMP
-	}
-
-	// create array from inputs to be used by displayNewRow function
-	var userArray = [userName, userCompany, userDressCode, userIndustry];
-	// userResult
-
-	// display each input value from userArray as a new row on DOM
-	displayNewRow(userArray);
-
-	// capture push function so that myKey returns the key of the specific object pushed
-	var myKey = database.ref().push(pushObject);
-
-	// do vize api call here
-
-	// redirect to results page & when api call is done append file parameters to URL
-	// window.location = "results.html?id=" + myKey.key + "&dressType=" + resultFromVize;
-
-}
-
-// function for hiding rows once data storage gets past 5 people
-function hideRows() {
-
-	$(".new-row").slice(5).css("display", "none");
-
-}
+};
 
 
 // MAIN PROCESS
 // ====================================================================================
-$(window).on("load", function() {
-
-	// flash user's new row on results page
-    $(".new-row:first-child").addClass("flash");
-
-})
-
-// when page loads
-database.ref().once("value", function(snapshot) {
-
-	// console insight
-	console.log(snapshot.val());
-
-	// initialize table
-	initPage(snapshot);
-
-	// hide rows past row 5
-	hideRows();
-
-// error handling
-}, function(err) {
-	console.error(err);
-});
-
-// whenever a new object is pushed to firebase
-database.ref().on("child_added", function(snapshot) {
-
-	// console insight
-	console.log(snapshot.val());
-
-// error handling
-}, function(err) {
-	console.error(err);
-});
-
 // when user clicks submit, execute submit function
 $("#submit").on("click", function(e) {
 
-	e.preventDefault();
+    e.preventDefault();
 
-	submit();
+    requestVizeAPI();
 
 });
-
